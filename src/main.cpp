37917e92a1cf06f8f31215c391def133c1f770b9
dormon <imilet@fit.vutbr.cz>
2023-10-20 14:19:45 +0200

    window

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,19 @@
 #include<iostream>
 
+#include<SDL.h>
+
 int main(int argc,char*argv[]){
+  SDL_CreateWindow("PGRe2023",0,0,1024,768,0);
+
+  bool running = true;
+  while(running){ // main loop
+    SDL_Event event;
+    while(SDL_PollEvent(&event)){ // event loop
+      if(event.type == SDL_QUIT)
+        running = false;
+
+    }
+  }
   std::cerr << "Hello World!" << std::endl;
   return 0;
 }
