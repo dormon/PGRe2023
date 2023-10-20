6ca4dea5e37d430a679a25a66bbe75f10378eb94
dormon <imilet@fit.vutbr.cz>
2023-10-20 15:49:40 +0200

    geGL added debugging

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,37 @@
+#include "geGL/DebugMessage.h"
+#include "geGL/OpenGL.h"
 #include<iostream>
 
 #include<SDL.h>
+#include<geGL/geGL.h>
+#include<geGL/StaticCalls.h>
+
+using namespace ge::gl;
 
 int main(int argc,char*argv[]){
+
   auto window = SDL_CreateWindow("PGRe2023",
       SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED,1024,768,SDL_WINDOW_OPENGL);
 
+  SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
+  SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 6);
+  SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, SDL_GL_CONTEXT_DEBUG_FLAG);
+  SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
   auto context = SDL_GL_CreateContext(window);
+
+  ge::gl::init();
+  ge::gl::setDefaultDebugMessage();
+
+
+  GLint major;
+  GLint minor;
+  glGetIntegerv(GL_MAJOR_VERSION,&major);
+  glGetIntegerv(GL_MINOR_VERSION,&minor);
+  std::cerr << major << minor << "0" << std::endl;
+  std::cerr << glGetString( GL_VERSION) << std::endl;
+
+  GLuint vao;
+  glCreateVertexArrays(1,&vao);
 
   bool running = true;
   while(running){ // main loop
@@ -32,20 +57,16 @@ int main(int argc,char*argv[]){
 
     // draw
  
-    using GLCLEARTYPE = void(*)(int);
-    using GLCLEARCOLORTYPE = void(*)(float,float,float,float);
-
-    GLCLEARTYPE      glClear      = nullptr;
-    GLCLEARCOLORTYPE glClearColor = nullptr;
-#define GL_COLOR_BUFFER_BIT			0x00004000
 
 
-    glClear      = (GLCLEARTYPE     )SDL_GL_GetProcAddress("glClear"     );
-    glClearColor = (GLCLEARCOLORTYPE)SDL_GL_GetProcAddress("glClearColor");
 
     glClearColor(0,.5,0,1);
     glClear(GL_COLOR_BUFFER_BIT);
 
+  
+    glBindVertexArray(vao);
+    glPointSize(10);
+    glDrawArrays(GL_POINTS,0,1);
 
     // after rendering
     SDL_GL_SwapWindow(window);
